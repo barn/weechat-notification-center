301fc1b1971e8f03efb595f001db737489d12bba
Ben Hughes <git@mumble.org.uk>
2015-05-05 17:11:48 -0700

    Make it not alert if you're looking at the buffer in question.
    And also, are not /away too.
    
    Tonne of pep8 style changes too. But whatever.

--- notification_center.py
+++ notification_center.py
@@ -1,48 +1,52 @@
-# https://github.com/sindresorhus/weechat-notification-center
+# https://github.com/barn/weechat-notification-center
 # Requires `pip install pync`
+# Original Author: Sindre Sorhus <sindresorhus@gmail.com>
+# Author: Ben Hughes <weechats@mumble.org.uk>
+# License: MIT
 
-import weechat
-from pync import Notifier
-
+import weechat as w
+from pync import Notifier as N
 
 SCRIPT_NAME = 'notification_center'
-SCRIPT_AUTHOR = 'Sindre Sorhus <sindresorhus@gmail.com>'
-SCRIPT_VERSION = '0.3.0'
+SCRIPT_AUTHOR = 'Ben Hughes <weechats@mumble.org.uk>'
+SCRIPT_VERSION = '0.4.0'
 SCRIPT_LICENSE = 'MIT'
-SCRIPT_DESC = 'Pass highlights and private messages to the OS X 10.8+ Notification Center'
+SCRIPT_DESC = 'Pass highlights & private messages to OSXs Notification Center'
 
-weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, '', '')
+w.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION,
+           SCRIPT_LICENSE, SCRIPT_DESC, '', '')
 
 DEFAULT_OPTIONS = {
-	'show_highlights': 'on',
-	'show_private_message': 'on',
-	'show_message_text': 'on',
-	'sound': 'off',
+    'show_highlights': 'on',
+    'show_private_message': 'on',
+    'show_message_text': 'on',
 }
 
 for key, val in DEFAULT_OPTIONS.items():
-	if not weechat.config_is_set_plugin(key):
-		weechat.config_set_plugin(key, val)
+        if not w.config_is_set_plugin(key):
+                w.config_set_plugin(key, val)
 
-weechat.hook_print('', 'irc_privmsg', '', 1, 'notify', '')
+w.hook_print('', 'irc_privmsg', '', 1, 'notify', '')
+
 
 def notify(data, buffer, date, tags, displayed, highlight, prefix, message):
-	# ignore if it's yourself
-	own_nick = weechat.buffer_get_string(buffer, 'localvar_nick')
-	if prefix == own_nick or prefix == ('@%s' % own_nick):
-		return weechat.WEECHAT_RC_OK
 
-	# passing `None` or `''` still plays the default sound so we pass a lambda instead
-	sound = 'Pong' if weechat.config_get_plugin('sound') == 'on' else lambda:_
-	if weechat.config_get_plugin('show_highlights') == 'on' and int(highlight):
-		channel = weechat.buffer_get_string(buffer, 'localvar_channel')
-		if weechat.config_get_plugin('show_message_text') == 'on':
-			Notifier.notify(message, title='%s %s' % (prefix, channel), sound=sound)
-		else:
-			Notifier.notify('In %s by %s' % (channel, prefix), title='Highlighted Message', sound=sound)
-	elif weechat.config_get_plugin('show_private_message') == 'on' and 'notify_private' in tags:
-		if weechat.config_get_plugin('show_message_text') == 'on':
-			Notifier.notify(message, title='%s [private]' % prefix, sound=sound)
-		else:
-			Notifier.notify('From %s' % prefix, title='Private Message', sound=sound)
-	return weechat.WEECHAT_RC_OK
+        # Skip alerting if you're already looking at it and you're not /away
+        if (buffer == w.current_buffer()
+           and w.buffer_get_string(buffer, 'localvar_away') == ''):
+            return w.WEECHAT_RC_OK
+
+        if w.config_get_plugin('show_highlights') == 'on' and int(highlight):
+                channel = w.buffer_get_string(buffer, 'localvar_channel')
+                if w.config_get_plugin('show_message_text') == 'on':
+                        N.notify(message, title='%s %s' % (prefix, channel))
+                else:
+                        N.notify('In %s by %s' % (channel, prefix),
+                                 title='Highlighted Message')
+        elif (w.config_get_plugin('show_private_message') == 'on'
+              and 'notify_private' in tags):
+                if w.config_get_plugin('show_message_text') == 'on':
+                        N.notify(message, title='%s [private]' % prefix)
+                else:
+                        N.notify('From %s' % prefix, title='Private Message')
+        return w.WEECHAT_RC_OK
